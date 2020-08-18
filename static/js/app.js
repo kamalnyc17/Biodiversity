cf7f319aa30830baaa80a69354412f5e794ca698
Kamal Mukherjee <kamalnyc17@gmail.com>
2020-08-18 18:29:07 -0400

    fixed the json access issue with localhost

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,8 @@
+// in order to import data from a json file in the local drive,
+// we need to start localhost server to avoid CORS error
+// from the terminal we need to type following command
+// python -m http.server
+
 // import data
-//import samples from '../../data/samples.json'
 const fileName = "./data/samples.json"
 d3.json(fileName).then(function(data){ console.log(data)});
