5bc17ee6e48642f0313024f1fa93d41f8d6625c3
Kamal Mukherjee <kamalnyc17@gmail.com>
2020-08-18 20:20:36 -0400

    completed dropdown

--- static/js/app.js
+++ static/js/app.js
@@ -3,6 +3,53 @@
 // from the terminal we need to type following command
 // python -m http.server
 
-// import data
+// global variables
 const fileName = "./data/samples.json"
-d3.json(fileName).then(function(data){ console.log(data)});
+let names, metadata, samples
+
+// build the dropdown
+d3.json(fileName).then(data => {
+  names = data.names
+  metadata = data.metadata
+  samples = data.samples}).then(l => {
+  const select = document.getElementById("selDataset");
+  names.map(name => {
+    let el = document.createElement("option")
+    el.textContent = el.value = name;
+    select.appendChild(el);
+  })
+  // building the dashboard based on the default value of the dropdown i.e. the first id of the list
+  optionChanged(document.getElementById("selDataset").value)
+}); 
+
+// main function to create dashboard
+const optionChanged = (id) => {
+  createDemogrphic(id)
+  barChart(id)
+  gaugeChart(id)
+  bubbleChart(id)
+}
+
+// Demographic Info
+const createDemogrphic = (id) => {
+  info = metadata.find(data => data.id.toString() === id)
+  document.getElementById('data-id').innerHTML = "ID: " + info.id
+  document.getElementById('data-ethnicity').innerHTML = "Ethnicity: " + info.ethnicity
+  document.getElementById('data-gender').innerHTML = "Gender: " + info.gender
+  document.getElementById('data-age').innerHTML = "Age: " + info.age
+  document.getElementById('data-location').innerHTML = "Location: " + info.location
+  document.getElementById('data-bbtype').innerHTML = "BBTYPE: " + info.bbtype
+  document.getElementById('data-wfreq').innerHTML = "WFREQ: " + info.wfreq  
+}
+// bar chart
+const barChart = (id) => {
+  console.log("bar chart", id, samples)  
+}
+// gauge chart
+const gaugeChart = (id) => {
+  console.log("gauge chart", id, samples)  
+}
+// bubble chart
+const bubbleChart = (id) => {
+  console.log("bubble chart", id, samples)  
+}
