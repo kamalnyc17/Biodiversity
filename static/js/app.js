f38f40506c168ded43a5ac87f6d8ce5ab7544689
Kamal Mukherjee <kamalnyc17@gmail.com>
2020-08-19 15:20:18 -0400

    bubble chart cont..

--- static/js/app.js
+++ static/js/app.js
@@ -124,4 +124,27 @@ const bubbleChart = (id) => {
   sample_values = info.sample_values.slice(0,10).reverse()
   otu_labels = info.otu_labels.slice(0,10).reverse()
   console.log("bubble chart", id, info)  
+
+  // plotting the chart
+  const trace1 = {
+    x: [1, 2, 3, 4],
+    y: [10, 11, 12, 13],
+    text: ['A<br>size: 40', 'B<br>size: 60', 'C<br>size: 80', 'D<br>size: 100'],
+    mode: 'markers',
+    marker: {
+      color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
+      size: [40, 60, 80, 100]
+    }
+  };
+  
+  const data = [trace1];
+  
+  const layout = {
+    title: 'Bubble Chart Hover Text',
+    showlegend: false,
+    height: 600,
+    width: 600
+  };
+  
+  Plotly.newPlot('bubble', data, layout);
 }
